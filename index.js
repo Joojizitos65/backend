67393cea12c18fefa4133c07f440f4383866b49e
NAROTTAM KIRTAN LENCINAZ CELESTRINO <narottam_celestrino@estudante.sesisenai.org.br>
2024-07-04 16:28:04 -0300

    comando get

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const express = require('express')
+const app = express()
+
+app.get('/teste-api', function(req, res){
+    res.send('Nossa api está funcionando')
+})
+
+app.listen(8000)
+
+
+
+
+
+
+
+
+
 function Filtrarpares(arrey){
     return arrey.filter(num => num % 2 === 0)
 }
